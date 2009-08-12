2d239ab0577cd1e39595f10d7f7fe8fe417d7b18
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-08-12 10:30:23 +0200

    Introduces arrow (:->) datatype name instead of Label to mimic functions.

--- Data/Record/Label.hs
+++ Data/Record/Label.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE TypeOperators #-}
 module Data.Record.Label
   ( Getter, Setter, Modifier
-  , Label (..)
+  , (:->) (..)
   , mkModifier
   , mkLabel
   , Lens (..)
@@ -24,7 +25,7 @@ type Getter   a b = a -> b
 type Setter   a b = b -> a -> a
 type Modifier a b = (b -> b) -> a -> a
 
-data Label a b = Label
+data a :-> b = Label
   { lget :: Getter   a b
   , lset :: Setter   a b
   , lmod :: Modifier a b
@@ -33,11 +34,11 @@ data Label a b = Label
 mkModifier :: Getter a b -> Setter a b -> Modifier a b
 mkModifier gg ss f a = ss (f (gg a)) a
 
-mkLabel :: Getter a b -> Setter a b -> Label a b
+mkLabel :: Getter a b -> Setter a b -> a :-> b
 mkLabel g s = Label g s (mkModifier g s)
 
 infixr 8 %
-(%) :: Label g a -> Label f g -> Label f a
+(%) :: (g :-> a) -> (f :-> g) -> (f :-> a)
 a % b = Label (lget a . lget b) (lmod b . lset a) (lmod b . lmod a)
 
 -- Apply custom `parser' and 'printer' function. This can be seen as a
@@ -46,23 +47,23 @@ a % b = Label (lget a . lget b) (lmod b . lset a) (lmod b . lmod a)
 class Lens f where
   lmap :: (a -> b, b -> a) -> f a -> f b
 
-instance Lens (Label f) where
+instance Lens ((:->) f) where
   lmap (f, g) (Label a b c) = Label (f . a) (b . g) (c . (g.) . (.f))
 
 -- Extend the state monad with support for labels.
 
-getM :: MonadState s m => Label s b -> m b
+getM :: MonadState s m => s :-> b -> m b
 getM = gets . lget
 
-setM :: MonadState s m => Label s b -> b -> m ()
+setM :: MonadState s m => s :-> b -> b -> m ()
 setM l = modify . lset l
 
-modM :: MonadState s m => Label s b -> (b -> b) -> m ()
+modM :: MonadState s m => s :-> b -> (b -> b) -> m ()
 modM l = modify . lmod l
 
 -- Run a state computation for a sub element updating this part of the state afterwards.
 
-enterM :: MonadState s m => Label s b -> State b b1 -> m b1
+enterM :: MonadState s m => s :-> b -> State b b1 -> m b1
 enterM l c = do
   b <- getM l
   let (a, s) = runState c b
@@ -71,31 +72,31 @@ enterM l c = do
 
 enterMT
   :: (MonadState s (t m), MonadTrans t, Monad m)
-  => Label s b -> StateT b m a -> t m a
+  => s :-> b -> StateT b m a -> t m a
 enterMT l c = do
   b <- getM l
   (a, s) <- lift $ runStateT c b
   setM l s
   return a
 
-bothM :: MonadState s m => Label s b -> State b b1 -> m (b, b1)
+bothM :: MonadState s m => s :-> b -> State b b1 -> m (b, b1)
 bothM parent cmp = do
   p <- getM parent
   c <- enterM parent cmp
   return (p, c)
 
-localM :: MonadState s m => Label s b -> m b1 -> m b1
+localM :: MonadState s m => s :-> b -> m b1 -> m b1
 localM l comp = do
   k <- getM l
   c <- comp
   setM l k
   return c
 
-withM :: MonadState s m => Label s b -> State b a -> m b1 -> m b1
+withM :: MonadState s m => s :-> b -> State b a -> m b1 -> m b1
 withM l c d = localM l (enterM l c >> d)
 
 -- Lift list indexing to a label.
 
-list :: Int -> Label [a] a
+list :: Int -> [a] :-> a
 list i = mkLabel (!! i) (\v a -> take i a ++ [v] ++ drop (i+1) a)
 
