78baed5f0413d78b02abef1ce652ee6054211e5b
Sebastiaan Visser <sfvisser@cs.uu.nl>
2009-09-02 00:47:53 +0100

    Cleaner interface.

--- Data/Record/Label.hs
+++ Data/Record/Label.hs
@@ -8,24 +8,17 @@ module Data.Record.Label
 
   -- * Label type.
   , (:->) (..)
-  , mkModifier
-  , mkLabel
-
-  -- * Identity and composition.
-  , idL
-  , (%)
+  , label
 
   -- * Bidirectional functor.
 
-  , Lens (..)
-  , (%%)
+  , Lens
+  , Iso (..)
+  , (%)
 
   -- * State monadic label operations.
 
   , getM, setM, modM, (=:)
-
-  -- * Convenient label for list indexing.
-  , list
 
   -- * Derive labels using Template Haskell.
   , module Data.Record.Label.TH
@@ -47,42 +40,34 @@ data a :-> b = Label
   , lmod :: Modifier a b
   }
 
--- | Create a modifier function out of a getter and a setter.
-
-mkModifier :: Getter a b -> Setter a b -> Modifier a b
-mkModifier gg ss f a = ss (f (gg a)) a
-
 -- | Smart constructor for `Label's, the modifier will be computed based on
 -- getter and setter.
 
-mkLabel :: Getter a b -> Setter a b -> a :-> b
-mkLabel g s = Label g s (mkModifier g s)
-
-idL :: a :-> a
-idL = mkLabel id const
-
-infixr 8 %
-(%) :: (g :-> a) -> (f :-> g) -> (f :-> a)
-a % b = Label (lget a . lget b) (lmod b . lset a) (lmod b . lmod a)
+label :: Getter a b -> Setter a b -> a :-> b
+label g s = Label g s (\f a -> s (f (g a)) a)
 
 instance Category (:->) where
-  id = idL
-  (.) = (%)
+  id = label id const
+  (Label ga sa ma) . (Label gb _ mb) = Label (ga . gb) (mb . sa) (mb . ma)
 
--- Apply custom `parser' and 'printer' function. This can be seen as a
--- bidirectional functorial map.
+type Lens a b = (a -> b, b -> a)
 
-class Lens f where
-  lmap :: (a -> b, b -> a) -> f a -> f b
+{- | Minimum definition is just one of the two. -}
 
-instance Lens ((:->) f) where
-  lmap (f, g) (Label a b c) = Label (f . a) (b . g) (c . (g.) . (.f))
+class Iso f where
+  iso :: Lens a b -> f a -> f b
+  iso (a, b) = osi (b, a)
+  osi :: Lens a b -> f b -> f a
+  osi (a, b) = iso (b, a)
+
+instance Iso ((:->) f) where
+  iso (f, g) (Label a b c) = Label (f . a) (b . g) (c . (g.) . (.f))
 
 -- | Apply label to lifted value and join afterwards.
 
-infixr 8 %%
-(%%) :: Functor f => a :-> b -> g :-> f a -> g :-> f b
-(%%) a b = let (Label g s _) = a in (fmap g, fmap (\k -> s k (error "unused"))) `lmap` b
+infixr 8 %
+(%) :: Functor f => a :-> b -> g :-> f a -> g :-> f b
+(%) a b = let (Label g s _) = a in (fmap g, fmap (\k -> s k (error "unused"))) `iso` b
 
 -- | Get a value out of state pointed to by the specified label.
 
@@ -107,7 +92,6 @@ modM :: MonadState s m => s :-> b -> (b -> b) -> m ()
 modM l = modify . lmod l
 
 -- Lift list indexing to a label.
-
-list :: Int -> [a] :-> a
-list i = mkLabel (!! i) (\v a -> take i a ++ [v] ++ drop (i+1) a)
+-- list :: Int -> [a] :-> a
+-- list i = label (!! i) (\v a -> take i a ++ [v] ++ drop (i+1) a)
 
