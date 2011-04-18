276324ee7f792db50963ea15be64b18c94b758ba
Sebastiaan Visser <sfvisser@cs.uu.nl>
2011-04-18 18:32:21 +0200

    Cleanup of TH code.

--- src/Data/Label/Derive.hs
+++ src/Data/Label/Derive.hs
@@ -1,4 +1,9 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS -fno-warn-orphans #-}
+{-# LANGUAGE
+    TemplateHaskell
+  , OverloadedStrings
+  , FlexibleInstances
+  #-}
 module Data.Label.Derive
 ( mkLabels
 , mkLabelsNoTypes
@@ -12,124 +17,126 @@ import Data.Function (on)
 import Data.Label.Abstract
 import Data.List
 import Data.Ord
+import Data.String
+import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 import Prelude hiding ((.), id)
+
+-- Throw a fclabels specific error.
+
+fclError :: String -> a
+fclError err = error ("Data.Label.Derive: " ++ err)
 
 -- | Derive lenses including type signatures for all the record selectors in a
 -- datatype.
 
 mkLabels :: [Name] -> Q [Dec]
-mkLabels = liftM concat . mapM (mkOneLabel True)
+mkLabels = liftM concat . mapM (derive1 True)
 
 -- | Derive lenses without type signatures for all the record selectors in a
 -- datatype.
 
 mkLabelsNoTypes :: [Name] -> Q [Dec]
-mkLabelsNoTypes = liftM concat . mapM (mkOneLabel False)
+mkLabelsNoTypes = liftM concat . mapM (derive1 False)
 
 -- Helpers to generate all labels.
 
-mkOneLabel :: Bool -> Name -> Q [Dec]
-mkOneLabel sigs name =
- do i <- reify name
+derive1 :: Bool -> Name -> Q [Dec]
+derive1 signatures datatype =
+ do i <- reify datatype
     let -- Only process data and newtype declarations, filter out all
         -- constructors and the type variables.
         (tyname, cons, vars) =
           case i of
             TyConI (DataD    _ n vs cs _) -> (n, cs,  vs)
             TyConI (NewtypeD _ n vs c  _) -> (n, [c], vs)
-            _                             -> fclError "Can only derive mkOneLabel for datatypes and newtypes."
+            _                             -> fclError "Can only derive labels for datatypes and newtypes."
 
         -- We are only interested in lenses of record constructors.
         recordOnly = groupByCtor [ (f, n) | RecC n fs <- cons, f <- fs ]
 
-    concat `liftM` mapM (codeGen sigs tyname vars (length cons)) recordOnly
+    concat `liftM` mapM (derive signatures tyname vars (length cons)) recordOnly
 
     where groupByCtor = map (\xs -> (fst (head xs), map snd xs))
                       . groupBy ((==) `on` (fst3 . fst))
                       . sortBy (comparing (fst3 . fst))
           fst3 (a, _, _) = a
 
-codeGen :: Bool -> Name -> [TyVarBndr] -> Int -> (VarStrictType, [Name]) -> Q [Dec]
-codeGen sigs tyname vars total ((field, _, fieldtyp), ctors) =
-   if total == length ctors
-     then label1
-     else label
+-- Generate the code for the labels.
+
+derive :: Bool -> Name -> [TyVarBndr] -> Int -> (VarStrictType, [Name]) -> Q [Dec]
+derive signatures tyname vars total ((field, _, fieldtyp), ctors) =
+
+  do (sign, body) <-
+       if length ctors == total
+       then function derivePureLabel
+       else function deriveMaybeLabel
+
+     return $
+       if signatures
+       then [sign, body]
+       else [body]
 
   where
 
-    -- Generate a name for the lens. If the original selector starts with an _,
-    -- remove it and make the next character lowercase. Otherwise, add 'l', and
-    -- make the next character uppercase.
-    name = mkName $
-            case nameBase field of
-              '_' : c : rest -> toLower c : rest
-              f : rest       -> 'l' : toUpper f : rest
-              _              -> fclError "Invalid name"
+    -- Generate a name for the label. If the original selector starts with an
+    -- underscore, remove it and make the next character lowercase. Otherwise,
+    -- add 'l', and make the next character uppercase.
+    fieldName = nameBase field
+    labelName = mkName $
+      case nameBase field of
+        '_' : c : rest -> toLower c : rest
+        f : rest       -> 'l' : toUpper f : rest
+        n              -> fclError ("Cannot derive label for record selector with name: " ++ n)
 
-    cases b = map (\ctor -> Match (RecP ctor []) (NormalB b) [] ) ctors
 
-    toyalTyName = foldr (flip AppT) (ConT tyname) (map tvToVarT (reverse vars))
+    -- Compute the type (including type variables of the record datatype.
+    inputType = return $ foldr (flip AppT) (ConT tyname) (map tvToVarT (reverse vars))
 
-    tvToVarT (PlainTV v) = VarT v
-    tvToVarT _           = fclError "No support for special-kinded type variables."
+    -- Convert a type variable binder to a regular type variable.
+    tvToVarT (PlainTV tv) = VarT tv
+    tvToVarT _            = fclError "No support for special-kinded type variables."
 
-    getterCases = cases getterBody ++ wild
-    setterCases = cases setterBody ++ wild
+    -- Q style record updating.
+    record rec fld val =
+      do v <- val
+         recUpdE rec [return (mkName fld, v)]
 
-    typ = 
-      do t <- [t| (ArrowChoice (~>), ArrowZero (~>)) => Lens (~>) $( return toyalTyName ) $( return fieldtyp ) |]
-         return (ForallT vars [] t)
+    -- Build a function declaration with both a type-signature and body.
+    function (s, b) = liftM2 (,) 
+        (sigD labelName s)
+        (funD labelName [ clause [] (normalB b) [] ])
 
-    wild = [Match (WildP ) (NormalB (ce "Left" `AppE` ce "()")) []]
+    -- Build a single record label definition for labels that might fail.
+    deriveMaybeLabel = (sign, body)
+      where
+        sign = forallT vars (return []) [t| (ArrowChoice (~>), ArrowZero (~>)) => Lens (~>) $(inputType) $(return fieldtyp) |]
+        body = [| let c = zeroArrow ||| returnA in lens (c . $(getter)) (c . $(setter)) |]
+          where
+            getter    = [| arr (\    p  -> $(caseE [|p|] (cases (bodyG [|p|]      ) ++ wild))) |]
+            setter    = [| arr (\(v, p) -> $(caseE [|p|] (cases (bodyS [|p|] [|v|]) ++ wild))) |]
+            cases b   = map (\ctor -> match (recP ctor []) (normalB b) []) ctors
+            wild      = [match wildP (normalB [| Left () |]) []]
+            bodyS p v = [| Right $( record p fieldName v ) |]
+            bodyG p   = [| Right $( fromString fieldName `appE` p ) |]
 
-    setterBody = ce "Right" `AppE` RecUpdE (ve "p") [(mkName (nameBase field), ve "v")]
-    getterBody = ce "Right" `AppE` (ve (nameBase field) `AppE` ve "p")
+    -- Build a single record label definition for labels that cannot fail.
+    derivePureLabel = (sign, body)
+      where
+        sign = forallT vars (return []) [t| Arrow (~>) => Lens (~>) $(inputType) $(return fieldtyp) |]
+        body = [| lens $(getter) $(setter) |]
+          where
+            getter = [| arr $(fromString fieldName) |]
+            setter = [| arr (\(v, p) -> $(record [| p |] fieldName [| v |])) |]
 
-    getter = ve "arr" `AppE` ([vp "p"               ] `LamE` CaseE (ve "p") getterCases)
-    setter = ve "arr" `AppE` ([TupP [vp "v", vp "p"]] `LamE` CaseE (ve "p") setterCases)
+-- IsString instances for TH types.
 
-    ce x = ConE (mkName x)
-    ve x = VarE (mkName x)
-    vp x = VarP (mkName x)
+instance IsString Exp where
+  fromString = VarE . mkName
 
-    body = [| let c = zeroArrow ||| returnA
-              in lens (c . $(return getter))
-                      (c . $(return setter))
-            |]
+instance IsString (Q Pat) where
+  fromString = varP . mkName
 
-    label = 
-      do t <- typ
-         b <- body
-         return
-           [ SigD name t
-           , FunD name [ Clause [] (NormalB b) [] ]
-           ]
-
-    -------------------------------------------------
-
-    label1 = 
-      do t1 <- typ1
-         b1 <- body1
-         return
-           [ SigD name t1
-           , FunD name [ Clause [] (NormalB b1) [] ]
-           ]
-
-    typ1 = 
-      do t <- [t| Arrow (~>) => Lens (~>) $( return toyalTyName ) $( return fieldtyp ) |]
-         return (ForallT vars [] t)
-
-    body1 = [| lens $(return getter1)
-                    $(return setter1)
-             |]
-
-    getter1 = ve "arr" `AppE` getterBody1
-    setter1 = ve "arr" `AppE` ([TupP [vp "v", vp "p"]] `LamE` setterBody1)
-
-    setterBody1 = RecUpdE (ve "p") [(mkName (nameBase field), ve "v")]
-    getterBody1 = ve (nameBase field)
-
-fclError :: String -> a
-fclError err = error ("Data.Label.Derive: " ++ err)
+instance IsString (Q Exp) where
+  fromString = varE . mkName
 
